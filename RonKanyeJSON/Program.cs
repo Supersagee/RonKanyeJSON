fc77ffb2f6b46e1423db08b11b3e2e584de79d47
Supersagee <williamdsage@gmail.com>
2022-03-13 23:38:36 -0500

    first go

---
+++ RonKanyeJSON/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+
+namespace RonKanyeJSON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var quote = new Quoter(client);
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("--------------------------------------------------------------------");
+                Console.WriteLine($"Ron: {quote.RonQuote()}");
+
+                Console.WriteLine("|||");
+                Console.WriteLine($"Kanye; {quote.KayneQuote()}");
+            }
+        }
+    }
+}
---
+++ RonKanyeJSON/Quoter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace RonKanyeJSON
+{
+    public class Quoter
+    {
+        private HttpClient _client;
+
+        public Quoter(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public string KayneQuote()
+        {
+            var kanyeURL = "https://api.kanye.rest";
+
+            var kanyeResponse = _client.GetStringAsync(kanyeURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            return kanyeQuote;
+        }
+
+        public string RonQuote()
+        {
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var ronResponse = _client.GetStringAsync(ronURL).Result;
+
+            var ronQuote = JArray.Parse(ronResponse);
+
+            return ronQuote[0].ToString();
+        }
+    }
+}
